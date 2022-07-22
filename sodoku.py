a73fc7272bcef29df7d871d0e6c2da3191e34bee
Jonathan Lee <79165906+jsmlee123@users.noreply.github.com>
2022-07-22 15:41:50 -0500

    sodoku outline

---
+++ sodoku.py
@@ -0,0 +1,36 @@
+import sys
+
+class sodoku:
+    def __init__(self):
+        self.board = [['_' for x in range(9)] for y in range(9)]
+        self.solution = None
+    def load_custom(self):
+        '''
+        '''
+
+    def generate_board(self):
+        '''
+        '''
+    
+    def set_val(self,x,y):
+        '''
+        '''
+    def delete_val(self,x,y):
+        '''
+        '''
+    def verify_board(self):
+        '''
+        '''
+    def _valid_row(self, i):
+        '''
+        '''
+    def _valid_column(self, i):
+        '''
+        '''
+    def _valid_group(self,i):
+        '''
+        '''
+    def solve_board(self):
+        '''
+        '''
+        
