ad89e738c1ab462dc655a606c30302858f8048e4
Jonathan Lee <79165906+jsmlee123@users.noreply.github.com>
2022-07-26 23:32:18 -0500

    more cleanup

--- Controller/sodoku_controller.py
+++ Controller/sodoku_controller.py
@@ -10,34 +10,45 @@ class SodokuController:
     '''
     Controller to interact between sodoku view and model
     '''
-    def __init__(self):
+    def __init__(self) -> None:
+        '''
+        Init view and model
+        '''
         self._app = QApplication(sys.argv)
         self._model = Sodoku()
         self._view = SodokuView()
         self.init()
 
-    def init(self):
+    def init(self) -> None:
+        '''
+        Set connections to view
+        '''
         self._view.update.connect(lambda : self.update_model())
         self._view.gen.connect(lambda : self.generate_model())
         self._view.reveal.connect(lambda : self.show_solution())
         self._view.check.connect(lambda : self.verify_solution())
         self._view.help.connect(lambda : self.get_hint())
 
-    def get_hint(self):
+    def get_hint(self) -> None:
+        '''
+        Update Model with hint and then updates view
+        '''
         self._model.get_hint()
         self.update_view()
         self._view.display()
     
     def verify_solution(self):
         '''
+        Verify that Solution is correct and tell user
         '''
         if self._model.is_solved():
-            self._view.is_solved.setText("Solved")
+            self._view.is_solved.setText("Solved!")
         else:
-            self._view.is_solved.setText("Unsolved")
+            self._view.is_solved.setText("Wrong!")
 
     def show_solution(self):
         '''
+        Give the solution to the view
         '''
         self._model.set_solved()
         self.update_view()
@@ -45,22 +56,29 @@ class SodokuController:
     
     def generate_model(self):
         '''
+        Generate a new model given on the current difficulty of 
+        the board
         '''
         self._model.generate_board(self._view.difficulty)
         self.update_view()
         self._view.display()
         self._view.is_solved.setText("")
 
-    def update_view(self):
+    def update_view(self) -> None:
+        '''
+        Update the view board with the model board
+        '''
         for i in range(9):
             for j in range(9):
                 model_cell = self._model._board[i][j]
-                view_cell = self._view._curr_board[i][j]
-                if model_cell != view_cell:
-                    self._view._curr_board[i][j] = model_cell
+                self._view._curr_board[i][j] = model_cell
         
 
-    def update_model(self):
+    def update_model(self) -> None:
+        '''
+        Update the model board with the view board. Ensure that
+        new values are valid, else return to old values
+        '''
         for i in range(9):
             for j in range(9):
                 model_cell = self._model._board[i][j]
@@ -71,7 +89,11 @@ class SodokuController:
                     else:
                         self.update_view()
                         self._view.display()
+                        
     def run(self):
+        '''
+        Run app
+        '''
         self._view.show()
         return self._app.exec_()
 
--- View/sodoku_view.py
+++ View/sodoku_view.py
@@ -6,28 +6,35 @@ from PyQt5.QtWidgets import *
 from Model.sodoku import Difficulty
 
 class SodokuView(QWidget):
+    '''
+    Init signals for inputs
+    '''
     update = pyqtSignal()
     gen = pyqtSignal()
     reveal = pyqtSignal()
     check = pyqtSignal()
     help = pyqtSignal()
+    
 
-    def __init__(self):
+    def __init__(self) -> None:
+        '''
+        Init non-gui members of class
+        '''
         super(SodokuView, self).__init__()
         self._curr_board = [['' for x in range(9)] for y in range(9)]
         self._cells = collections.defaultdict(QLineEdit)
-        self.difficulty = Difficulty.MEDIUM
+        self.difficulty = Difficulty.EASY
         self._initUI()
         
 
-    def _initUI(self):
+    def _initUI(self) -> None:
         '''
         TODO:
             9x9 board in center of application, each should be text box to enter input - Done
-            Generate board button 
-            Check board button
-            Hint button
-            Show solution button
+            Generate board button - Done
+            Check board button - Done
+            Hint button - Done
+            Show solution button - Done
             *Load custom board
         '''
         #Add background image
@@ -42,7 +49,9 @@ class SodokuView(QWidget):
         title.setFont(QFont('Times', 40))
         title.move(365,50)
 
+        #combobox to choose difficulties
         self.combobox1 = QComboBox(self)
+        self.combobox1.currentTextChanged.connect(self.change_difficulty)
         self.combobox1.resize(100,30)
         self.combobox1.move(100,540)
         self.combobox1.addItem('Easy')
@@ -81,17 +90,38 @@ class SodokuView(QWidget):
         self.is_solved.resize(500,100)
         self.is_solved.move(260, 650)
 
+        #reveal widget
         self.show()
+    
+    def change_difficulty(self, diff : str):
+        '''
+        Change current set difficulty in view
+        '''
+        if diff == "Easy":
+            self.difficulty = Difficulty.EASY
+        elif diff == "Medium":
+            self.difficulty = Difficulty.MEDIUM
+        else:
+            self.difficulty = Difficulty.HARD
 
-    def set_curr_board(self, board):
+    def set_curr_board(self, board : list[list[str]]) -> None:
+        '''
+        Change the current board to the given one
+        '''
         self._curr_board = board
 
     def display(self):
+        '''
+        Sets the cells in the board to current one
+        '''
         for i in range(9):
             for j in range(9):
                 self._cells[(i,j)].setText(self._curr_board[i][j])
 
-    def update_board(self):
+    def update_board(self) -> None:
+        '''
+        Updates the view board, if the input on the board was correct
+        '''
         for i in range(9):
             for j in range(9):
                 cell = self._cells[(i,j)]
@@ -101,7 +131,10 @@ class SodokuView(QWidget):
                     else:
                         cell.setText(self._curr_board[i][j])
 
-    def init_sodoku(self):
+    def init_sodoku(self) -> None:
+        '''
+        Init the sodoku board graphic cells 
+        '''
         for i in range(9):
             for j in range(9):
                 temp = QLineEdit(self)
